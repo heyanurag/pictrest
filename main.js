f2f10e714a36d6887e6056190faf094d0df39f19
Anurag Singh <sa.anuragsingh@gmail.com>
2022-01-21 17:27:30 +0530

    initial setup

---
+++ main.js
@@ -0,0 +1,11 @@
+const form = document.querySelector(".form");
+const input = document.querySelector("input");
+
+// console.log(query.value);
+
+const searchPhotos = (query) => {};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchPhotos(input.value);
+});
