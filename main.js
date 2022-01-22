d4d82e9fee3a8f3a27dcd2338869a6cbb7f12d2e
Anurag Singh <sa.anuragsingh@gmail.com>
2022-01-22 22:21:25 +0530

    masonry layout works!

--- main.js
+++ main.js
@@ -2,21 +2,56 @@ import "./style.css";
 
 const form = document.querySelector(".form");
 const input = document.querySelector("input");
+const container = document.querySelector(".container");
+const template = document.getElementsByTagName("template")[0].content;
+const imgTemplate = template.querySelector("img");
 
 // console.log(query.value);
 
-const searchPhotos = async (query) => {
+const getCanvasElement = () => {
+  const canvas = document.createElement("div");
+  canvas.id = "canvas";
+  return canvas;
+};
+
+const displayPhotos = (photos) => {
+  const canvas = getCanvasElement();
+  console.log(photos);
+  photos.forEach((photo) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    imgTemplate.src = photo.url;
+    imgTemplate.alt = photo.description;
+
+    const imgTag = document.importNode(template, true);
+
+    card.appendChild(imgTag);
+    canvas.appendChild(card);
+  });
+
+  container.appendChild(canvas);
+};
+
+const getPhotos = async (query) => {
   const response = await fetch("/.netlify/functions/getPhotos", {
     method: "POST",
     body: JSON.stringify({
       query,
     }),
   });
-  const json = await response.json();
-  console.log(json);
+
+  const photos = await response.json();
+  console.log(photos);
+
+  return photos;
 };
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  searchPhotos(input.value);
+  if (document.getElementById("canvas")) {
+    document.getElementById("canvas").remove();
+  }
+  const photos = await getPhotos(input.value);
+  displayPhotos(photos);
 });
